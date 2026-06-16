c2761b35bef8c8252731fd3a2fdc0b52dbabbc20
Garry <61240122+curryhendry@users.noreply.github.com>
2026-06-16 11:52:16 +0800

    dev: 0.3.228

--- src/main.js
+++ src/main.js
@@ -3755,6 +3755,11 @@ module.exports = class CloudAttachPlugin extends Plugin {
       const { Notice: Notice2 } = require("obsidian");
       new Notice2("\u{1F50D} \u5168\u5C4F\u9884\u89C8\u529F\u80FD\uFF0C\u656C\u8BF7\u671F\u5F85");
     };
+    const versionBadge = document.createElement("span");
+    versionBadge.textContent = "v227";
+    versionBadge.style.cssText = "font-size:9px;opacity:0.5;cursor:default;";
+    versionBadge.title = "CloudAttach \u7248\u672C\u53F7";
+    toolbar.appendChild(versionBadge);
     const scrollArea = container.querySelector(".cloudattach-pdf-scrollarea");
     const scrollToPage = (pageNum) => {
       const targetCanvas = scrollArea.querySelector(`.cloudattach-pdf-page[data-page-num="${pageNum}"]`);
