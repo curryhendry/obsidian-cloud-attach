f3566f50d1038e3ac213fdfd9b761265dedd827c
Garry <61240122+curryhendry@users.noreply.github.com>
2026-06-16 14:37:36 +0800

    dev: 0.3.237

--- main.js
+++ main.js
@@ -3286,7 +3286,6 @@ module.exports = class CloudAttachPlugin extends Plugin {
       if (computedOpacity !== "1") {
         existingContainer.style.setProperty("opacity", "1", "important");
       }
-      this._renderingPdfUrls.delete(url);
       return;
     }
     while (this._pdfRendering) {
@@ -3536,7 +3535,7 @@ module.exports = class CloudAttachPlugin extends Plugin {
       new Notice2("\u{1F50D} \u5168\u5C4F\u9884\u89C8\u529F\u80FD\uFF0C\u656C\u8BF7\u671F\u5F85");
     };
     const versionBadge = document.createElement("span");
-    versionBadge.textContent = "v234";
+    versionBadge.textContent = "v237";
     versionBadge.style.cssText = "font-size:9px;opacity:0.5;cursor:default;";
     versionBadge.title = "CloudAttach \u7248\u672C\u53F7";
     toolbar.appendChild(versionBadge);
